f9f8b504171fcb85bd256ad70c3414a1210b2f99
Yurii Holubchenko <yurii.holubchenko@gmail.com>
2024-07-28 16:03:06 +0200

    Add solution for 2235. Add Two Integers

---
+++ 2235.add-two-integers.rb
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/add-two-integers/
+
+# Test cases:
+# num1 = 12, num2 = 5
+# num1 = -10, num2 = 4
+
+# Expected results:
+# 17
+# -6
+
+# @param {Integer} num1
+# @param {Integer} num2
+# @return {Integer}
+
+def sum(num1, num2)
+  num1 + num2
+end
